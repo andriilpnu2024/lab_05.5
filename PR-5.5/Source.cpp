7050f93bce1396e3e7b8dae23052771e445a9d0d
andriilpnu2024 <andrii.kashchuk.ri.2024@lpnu.ua>
2024-10-09 13:38:19 +0300

    Second master commit

--- PR-5.5/Source.cpp
+++ PR-5.5/Source.cpp
@@ -4,9 +4,8 @@
 
 using namespace std;
 
-double x1(const double a, const double b, const double eps, int depth);
+double x1(const double a, const double b, const double eps, int &depth,int level);
 double f(const double a);
-
 int main()
 {
     double a;
@@ -17,7 +16,8 @@ int main()
     cout << "a= "; cin >> a;
     cout << "b= "; cin >> b;
     cout << "eps= "; cin >> eps;
-    x = x1(a, b, eps, depth);
+    x = x1(a, b, eps, depth ,1);
+    cout << "depth= " << depth << endl;
     cout << "x= " << x << endl;
     return 0;
 }
@@ -25,23 +25,25 @@ int main()
 double f( double a){
     return sin(a) - cos(a);
 }
-double x1(double a,double b, const double eps, int depth){
+double x1(double a,double b, const double eps, int &depth, int level){
     double m;
     m = (a + b) / 2;
-    depth++;
+    if (level > depth)
+        depth = level;
+    cout << " level = " << level << endl;
     if (f(m) == 0) {
-        cout << "depth= " << depth << endl;
+        cout << "level= " << depth << endl; 
         return m;
    }
-    if (fabs(b-a) <= 2 * eps) {
-        cout << "depth= " << depth << endl;
+    if (fabs(b-a) <= 2 * eps) { 
+        cout << "level= " << depth << endl; 
         return m;
    }
    if((f(a) * f(m)) < 0){
-       return x1(a, m, eps,depth);
+       return x1(a, m, eps,depth,level + 1); 
    }
        
    if ((f(b) * f(m)) < 0) {
-       return x1(m, b, eps,depth);
+       return x1(m, b, eps,depth,level + 1); 
    }
 }
